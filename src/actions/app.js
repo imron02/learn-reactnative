16bdd874da84fbfc6ecaf0aaab74f76f4a010be4
Imron Rosdiana <imron.rosdiana@dataon.com>
2018-01-24 19:07:08 +0700

    Add root page action & auth action

---
+++ src/actions/app.js
@@ -0,0 +1,17 @@
+import * as types from '../constants/app';
+
+export function appInitialized() {
+  return async function (dispatch, getState) {
+    dispatch(changeAppRoot('login'));
+  };
+}
+
+export function changeAppRoot(root) {
+  return { type: types.ROOT_CHANGED, root: root };
+}
+
+export function appDashboard() {
+  return async function (dispatch, getState) {
+    dispatch(changeAppRoot('dashboard'));
+  };
+}
---
+++ src/actions/auth.js
@@ -0,0 +1,14 @@
+
+export function login(username, password) {
+  return {
+    type: 'LOGIN',
+    username: username,
+    password: password
+  };
+};
+
+export const logout = () => {
+  return {
+    type: 'LOGOUT'
+  };
+};
