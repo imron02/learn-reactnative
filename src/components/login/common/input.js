feed8053ebadfd09e56b40c36c1608de7c1fc226
Imron Rosdiana <imron.rosdiana@dataon.com>
2018-01-24 21:37:02 +0700

    Update login form by component

---
+++ src/components/login/common/input.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+class Input extends Component {
+
+  render() {
+    const {
+      inputRef,
+      placeholder,
+      keyboardType,
+      autoCorrect,
+      secureTextEntry,
+      returnKeyType,
+      onSubmitEditing,
+      onChangeText
+    } = this.props;
+
+    return (
+      <TextInput
+        ref={inputRef}
+        placeholder={placeholder}
+        placeholderTextColor="rgba(255,255,255,0.7)"
+        underlineColorAndroid='rgba(0,0,0,0)'
+        keyboardType={keyboardType}
+        autoCorrect={autoCorrect}
+        secureTextEntry={secureTextEntry}
+        returnKeyType={returnKeyType}
+        onSubmitEditing={onSubmitEditing}
+        onChangeText={onChangeText}
+        style={styles.input}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    marginBottom: 20,
+    color: '#FFF',
+    paddingHorizontal: 10
+  }
+});
+
+export { Input };
--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  StatusBar
+  View
 } from 'react-native';
 import { connect } from 'react-redux';
 import { login } from '../../actions/auth';
 import { appDashboard } from '../../actions/app';
+import { Input, Buttons } from './common';
 
 class LoginForm extends Component {
   constructor(props) {
@@ -25,53 +22,39 @@ class LoginForm extends Component {
     this.props.onLogin(this.state.username, this.state.password);
 
     // Change root view
-    console.log(this.props, 444);
-    this.props.goToDashboard();
+    // this.props.goToDashboard();
   }
 
   _register = () => {
     this.props.navigator.push({
       screen: 'flutterasia.RegisterScreen',
-      title: 'Register',
-      navigatorStyle: {
-        navBarTextColor: 'rgba(255,255,255,0.7)',
-        navBarBackgroundColor: '#3498db',
-        navBarButtonColor: 'rgba(255,255,255,0.7)',
-        statusBarTextColorScheme: 'light'
-      }
+      title: 'Register'
     });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
+        <Input
           placeholder="Username or email"
-          placeholderTextColor="rgba(255,255,255,0.7)"
-          underlineColorAndroid='rgba(0,0,0,0)'
           keyboardType="email-address"
           autoCorrect={false}
           returnKeyType="next"
           onSubmitEditing={() => this.passwordInput.focus()}
-          onChangeText={(text) => this.setState({ username: text })}
-          style={styles.input}
-        />
-        <TextInput
+          onChangeText={(text) => this.setState({ username: text })} />
+        <Input
+          inputRef={(input) => this.passwordInput = input}
           placeholder="Password"
-          placeholderTextColor="rgba(255,255,255,0.7)"
-          underlineColorAndroid='rgba(0,0,0,0)'
           secureTextEntry={true}
           returnKeyType="go"
-          ref={(input) => this.passwordInput = input}
-          onChangeText={(text) => this.setState({ password: text })}
-          style={styles.input}
-        />
-        <TouchableOpacity style={styles.buttonLogin} onPress={this._login}>
-          <Text style={styles.buttonTextLogin}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this._register}>
-          <Text style={styles.buttonTextLogin}>New to Flutter Asia? Sign up</Text>
-        </TouchableOpacity>
+          onChangeText={(text) => this.setState({ password: text })} />
+        <Buttons 
+          styleBtn={true}
+          label="LOGIN"
+          click={this._login} />
+        <Buttons 
+          label="New to Flutter Asia? Sign up"
+          click={this._register} />
       </View>
     );
   }
@@ -80,23 +63,6 @@ class LoginForm extends Component {
 const styles = StyleSheet.create({
   container: {
     padding: 20
-  },
-  input: {
-    height: 40,
-    backgroundColor: 'rgba(255,255,255,0.2)',
-    marginBottom: 20,
-    color: '#FFF',
-    paddingHorizontal: 10
-  },
-  buttonLogin: {
-    backgroundColor: '#2980b9',
-    paddingVertical: 15,
-    marginBottom: 10
-  },
-  buttonTextLogin: {
-    textAlign: 'center',
-    fontWeight: '700',
-    color: '#FFF'
   }
 });
 
