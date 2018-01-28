7774a15f5ce2d09899de900f4f954e7c08673ca3
Imron Rosdiana <imron.rosdiana@dataon.com>
2018-01-28 17:04:01 +0700

    Setting react native navigation for android

--- android/app/src/main/java/com/flutterasia/MainActivity.java
+++ android/app/src/main/java/com/flutterasia/MainActivity.java
@@ -1,15 +1,15 @@
 package com.flutterasia;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "flutterasia";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "flutterasia";
+    // }
 }
