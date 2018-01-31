4ab6419e454d2c332b911942b640a9ab5920f53f
Imron Rosdiana <imron.rosdiana@dataon.com>
2018-01-31 13:32:10 +0700

    Fix error react native link

--- android/app/src/main/java/com/flutterasia/MainApplication.java
+++ android/app/src/main/java/com/flutterasia/MainApplication.java
@@ -15,38 +15,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
-
-  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-  //   @Override
-  //   public boolean getUseDeveloperSupport() {
-  //     return BuildConfig.DEBUG;
-  //   }
-
-  //   @Override
-  //   protected List<ReactPackage> getPackages() {
-  //     return Arrays.<ReactPackage>asList(
-  //         new MainReactPackage(),
-            new PickerPackage()
-  //     );
-  //   }
-
-  //   @Override
-  //   protected String getJSMainModuleName() {
-  //     return "index";
-  //   }
-  // };
-
-  // @Override
-  // public ReactNativeHost getReactNativeHost() {
-  //   return mReactNativeHost;
-  // }
-
-  // @Override
-  // public void onCreate() {
-  //   super.onCreate();
-  //   SoLoader.init(this, /* native exopackage */ false);
-  // }
-
   @Override
   public boolean isDebug() {
     // Make sure you are using BuildConfig from your own application
@@ -58,6 +26,7 @@ public class MainApplication extends NavigationApplication {
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
     // eg. new VectorIconsPackage()
+      new PickerPackage()
     );
   }
 
